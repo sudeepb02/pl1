216eee2037315e99e9f7f3a18465398b6ed4c65f
Sudeep <sudeepbiswas02@gmail.com>
2016-07-13 02:15:54 +0000

    Program implemented using files

--- socketprog/clientprog.py
+++ socketprog/clientprog.py
@@ -5,7 +5,16 @@ c = socket.socket()
 hname = socket.gethostname()
 port = 1025
 
-c.connect((hname,port))
+c.connect(("localhost",port))
 
-print c.recv(1025)
+#print(c.recv(1024))
+
+f = open('recdfile.txt','wb')
+
+while(True):
+	l = c.recv(1024)
+	while(l):
+		f.write(l)
+		l = sc.recv(1024)
+f.close()
 c.close()
--- socketprog/serverprog.py
+++ socketprog/serverprog.py
@@ -4,12 +4,21 @@ import socket,sys
 s = socket.socket()
 hostname = socket.gethostname()
 port = 1025
-s.bind((hostname,port))
+s.bind(("localhost",port))
 
-s.listen(1)
+s.listen(5)
 c,addr = s.accept()
-print 'Client connected ; ',addr
-c.send('Request accepted')
+
+print('Client connected : ',addr)
+#c.send('Request accepted')
+
+f = open("orgfile.txt","rb")
+l = f.read(1024)
+
+while(l):
+	s.send(l)
+	l = f.read(1024)
+
 c.close()
 
 
